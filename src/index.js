9ed92408587b81271be336b80d9fc418f67b52ee
yokota-daiki <ribrate78@gmail.com>
2022-11-18 16:33:13 +0900

    jsファイルでcssを適用

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import ReactDOM from "react-dom";
-
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは!</h1>
-      <p>お元気ですか?</p>
-    </>
-  );
-};
+import { App } from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
