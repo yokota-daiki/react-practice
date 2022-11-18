61cc6e06b7a358bf01aedd21379dfc3e1c8ce342
yokota-daiki <ribrate78@gmail.com>
2022-11-18 17:47:34 +0900

    propsの省略記法

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,8 +1,9 @@
 export const ColoredMessage = (props) => {
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color,
+    color,
     fontSize: "20px"
   };
 
-  return <p style={contentStyle}>{props.children}</p>
+  return <p style={contentStyle}>{children}</p>
 }
