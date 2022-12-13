2cea5b719d1204d825c1d5a9b401921615d30d27
yokota-daiki <ribrate78@gmail.com>
2022-12-13 17:28:21 +0900

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./components/CssModules";
 
 export const App = () => {
   const [num, setNum] = useState(0);
