02708c8caa50dba3cfbff4de8902b619536dedc9
yokota-daiki <ribrate78@gmail.com>
2022-12-29 20:11:29 +0900

    yarn経由でtailwindを導入

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { CssModules } from "./components/CssModules";
 import { StyledJsx } from "./components/StyledJsx";
 import { StyledComponents } from "./components/StyledComponents";
 import { Emotion } from "./components/Emotion";
+import { TailwindCss } from "./components/TailwindCss";
 
 
 export const App = () => {
@@ -22,6 +23,7 @@ export const App = () => {
       <StyledJsx/>
       <StyledComponents/>
       <Emotion/>
+      <TailwindCss/>
     </>
   );
 };
